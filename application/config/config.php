9d32f9236b20eb5bea19bbf15ccc33690d667e1c
parinya <parinya.coj@gmail.com>
2018-08-03 13:01:58 +0700

    [Edited] - แก้ไข base_url , แก้ไข database name

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://cdc.coj.intra/';
-$config['base_url'] = 'http://localhost:8888/cojnews/';
+$config['base_url'] = 'http://localhost:8888/appro/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/logs/log-2018-08-03.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2018-08-03 07:59:18 --> UTF-8 Support Enabled
+DEBUG - 2018-08-03 07:59:19 --> No URI present. Default controller set.
+DEBUG - 2018-08-03 07:59:19 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-08-03 07:59:20 --> Encryption: Auto-configured driver 'mcrypt'.
+DEBUG - 2018-08-03 07:59:20 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2018-08-03 07:59:20 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2018-08-03 07:59:20 --> Total execution time: 1.5095
+DEBUG - 2018-08-03 07:59:21 --> UTF-8 Support Enabled
+DEBUG - 2018-08-03 07:59:21 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2018-08-03 07:59:21 --> 404 Page Not Found: Images/favicon.png
